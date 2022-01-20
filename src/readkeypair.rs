703eaa3c552a9c124601974f337c7d04904b3314
Chris <chris14649@gmail.com>
2022-01-20 16:21:22 +0800

    update readkeypair

--- src/readkeypair.rs
+++ src/readkeypair.rs
@@ -5,8 +5,5 @@ fn main() {
 
     let kriss = read_keypair_file("/Users/chris/.config/solana/id.json");
     println!("Kirss Publickey : {:?}",kriss.unwrap().pubkey());
-
-    let chris = read_keypair_file("/Users/chris/.config/solana/ag.json");
-    println!("Chris Publickey : {:?}",chris.unwrap().pubkey());
     
 }
